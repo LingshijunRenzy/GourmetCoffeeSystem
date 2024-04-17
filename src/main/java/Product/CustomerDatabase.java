ab98dca0ff6291cc0b3896de31c6c6f0851d0dd6
HJY1029 <139771395+HJY1029@users.noreply.github.com>
2024-04-17 10:19:28 +0800

    Create CustomerDatabase.java

---
+++ src/main/java/Product/CustomerDatabase.java
@@ -0,0 +1,94 @@
+package Product;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * Maintains a collection of {@link Customer} objects.
+ *
+ * @author A Jiayi
+ * @version  1.0.0
+ * @see Customer
+ */
+public class CustomerDatabase {
+
+	/* Collection of <code>Customer</code> objects.*/
+	private Vector<Customer> customers;
+
+	/**
+	 * Constructs an empty collection of {@link Customer}
+	 * objects.
+	 */
+	public CustomerDatabase() {
+
+		customers = new Vector<Customer>();
+	}
+
+	/**
+	 * Adds a {@link Customer} object to this collection.
+	 *
+	 * @param customer  the {@link Customer} object.
+	 */
+	public void  addCustomer(Customer customer) {
+
+		Customer.add(customer);
+	}
+
+	/**
+	 * Returns an iterator over the customers in this database.
+	 *
+	 * return  an {@link Iterator}
+	 */
+	public Iterator<Customer>  getCustomersIterator() {
+
+		return customers.iterator();
+	}
+
+	/**
+	 * Returns the {@link Customer} object with the specified
+	 * <code>id</code>.
+	 *
+	 * @param code  the id of the customer.
+	 * @return  The {@link Customer} object with the specified id.
+	 *          Returns <code>null</code> if the object with the
+	 *          id is not found.
+	 */
+	/*
+	 * public Customer getCustomer(String id) {
+	 * 
+	 * for (Iterator<Customer> i = getCustomersIterator(); i.hasNext();) {
+	 * 
+	 * Customer customer = (Customer) i.next();
+	 * 
+	 * if (customer.getId().equals(id)) {
+	 * 
+	 * return customer; } }
+	 * 
+	 * return null; }
+	 */
+
+	/**
+	 * Returns the number of {@link Customer} objects in this collection.
+	 *
+	 * @return  the number of {@link Customer} objects in this collection.
+	 */
+	public int  getNumberOfCustomers() {
+
+		return ProductCatalog.getNumberOfItems();
+	}
+
+	public Customer getCustomer(String line) {
+		
+		for (Iterator<Customer> i = getCustomersIterator(); i.hasNext();) {
+
+			Customer borrower = (Customer) i.next();
+
+			if (borrower.getId().equals(id)) {
+
+				return borrower;
+			}
+		}
+
+		return null;
+	}
+}
