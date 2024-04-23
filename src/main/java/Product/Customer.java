866f74cde490dec23d48277c228d798ae7d43b70
HJY1029 <139771395+HJY1029@users.noreply.github.com>
2024-04-23 20:41:58 +0800

    Update Customer.java

--- src/main/java/Product/Customer.java
+++ src/main/java/Product/Customer.java
@@ -1,151 +1,114 @@
 package Product;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.UUID;
-import java.util.Vector;
+public class Coffee extends Product {
 
-public class Customer {
-	private String  id;
-	private String  name;
-	private int points;
+    private String acidity;
 
-	private Vector<Order> orders = new Vector<Order>();
+    private String aroma;
 
+    private String body;
 
+    private String OriginCountry;
 
-	/**
-	 * Constructs a <code>Customer</code> object.<p>
-	 * The collection of the borrowed items is initially empty.</p>
-	 *
-	 * @param id  the id of the customer.
-	 * @param name  the name of the customer.
-	 * @param points the points of the customer.
-	 */
-	public Customer(String id, String name, int points) {
-		this.id = id;
-		this.name = name;
-		this.points = points;
-	}
+    private String TypeofRoast;
+
+    private String flavor;
+
+    // 其他特定于咖啡或冲泡机的属性可以在子类中定义
+
+    public Coffee(String code, String description, double price, String acidity, String aroma, String OriginCountry,
+                  String body, String TypeofRoast, String flavor) {
+        super(code,description, price);
 
 
+        this.acidity = acidity;
 
-	/**
-	 * Returns the identification number of this customer.
-	 *
-	 * @return  the identification number of this customer.
-	 */
-	public String getId()  {
-		return  id;
-	}
+        this.aroma = aroma;
 
+        this.body = body;
 
+        this.OriginCountry = OriginCountry;
 
-	/**
-	 * Returns <code>true</code> if the id of this borrower is
-	 * equal to the id of the argument.
-	 *
-	 * @param object  object with which this borrower is compared.
-	 * @return  <code>true</code> if the id of this borrower is
-	 *          equal to the id of the argument; <code>false</code>
-	 *          otherwise.
-	 */
-	public boolean equals(Object object) {
+        this.TypeofRoast = TypeofRoast;
 
-		return object instanceof Customer
-		       && getId().equals(((Customer) object).getId());
-	}
+        this.flavor = flavor;
 
-    
-    
-  
-    public String getName() {  
-        return name;  
-    }  
-  
-    public int getPoints() {  
-        return points;  
-    }  
-  
-    public void addPoints(int pointsToAdd) {  
-        this.points += pointsToAdd;  
-    }  
-  
-    public void deductPoints(int pointsToDeduct) {  
-        if (pointsToDeduct <= points) {  
-            this.points -= pointsToDeduct;  
-        } else {  
-            System.out.println("Insufficient points for deduction.");  
-        }  
-    }  
-
-
-
-    @Override  
-    public String toString() {  
-        return "Customer{" +  
-                "name='" + name +
-                ", points=" + points +  
-                '}';  
     }
 
+    // Getter 和 Setter 方法
 
+    public String getCode() {
+        return super.getCode();
 
-	public void addOrder(Order order) {
-		orders.add(order);
-	}
+    }
 
-	public void removeOrder(UUID orderID) {
-		orders.removeIf(o -> o.getOrderID().equals(orderID));
-	}
+    public void setCode(String code) {
+        super.setCode(code);
+    }
 
-	public Order getOrder(UUID orderID) {
-		for (Order o : orders) {
-			if (o.getOrderID().equals(orderID)) {
-				return o;
-			}
-		}
-		return null;
-	}
+    public String getDescription() {
+        return super.getDescription();
+    }
 
-	public int getNumberOfOrders() {
-		return orders.size();
-	}
+    public void setDescription(String description) {
+        super.setDescription(description);
+    }
 
-	public Iterator<Order> getOrdersIterator() {
-		return orders.iterator();
-	}
+    public double getPrice() {
+        return super.getPrice();
+    }
 
+    public void setPrice(double price) {
+        super.setPrice(price);
+    }
 
-	/**
-	 * Buys a product and create a new order.
-	 *
-	 * @param product  the product to buy.
-	 * @param quantity the quantity of the product to buy.
-	 */
-	public void buy(Product product, int quantity) {
-		Order order = new Order();
-		order.addProduct(product, quantity);
-		addOrder(order);
-		OrderDataBase.addOrder(order);
-	}
+    public void getOriginCountry(String OriginCountry) {
 
+        this.OriginCountry = OriginCountry;
 
-	/**
-	 * Buys a list of products and create a new order.
-	 * The number of products and quantities must be the same.
-	 * @param products
-	 * @param quantities
-	 */
-	public void buy(List<Product> products, List<Integer> quantities) {
-		if(products.size() != quantities.size()) {
-			throw new IllegalArgumentException("The number of products and quantities must be the same.");
-		}
-		Order order = new Order();
-		for (int i = 0; i < products.size(); i++) {
-			order.addProduct(products.get(i), quantities.get(i));
-		}
-		addOrder(order);
-		OrderDataBase.addOrder(order);
-	}
+    }
+
+    public void getTypeofRoast(String TypeofRoast) {
+
+        this.TypeofRoast = TypeofRoast;
+
+    }
+
+    public void getFlavor(String flavor) {
+
+        this.flavor = flavor;
+
+    }
+
+    public void getAroma(String aroma) {
+        this.aroma = aroma;
+    }
+
+    public void getAcidity(String acidity) {
+        this.acidity = acidity;
+    }
+
+    public void getBody(String body) {
+        this.body = body;
+    }
+
+    // toString 方法用于打印产品信息
+
+    @Override
+
+    public String toString() {
+
+        return "Product{" +
+
+                "code='" + super.getCode() + '\'' +
+
+                ", description='" + super.getDescription() + '\'' +
+
+                ", price=" + super.getPrice() +
+
+                ", Country of origin=" + OriginCountry + ", acidity:" + acidity + "' aroma:" + aroma + ",body:" + body
+                + ",OriginCountry:" + OriginCountry + ",Type of Roast:" + TypeofRoast + ",flavor:" + flavor + '}';
+
+    }
+
 }
